be8b2ea52553c77b35f588a0a755cd3bf3fc7e17
Tom Schraitle <tom_schr@web.de>
2025-05-08 11:39:16 +0200

    Fix a implementation problem in Product and dashes
    Dashes are a common character in our product names. As such, we need
    to take care that the Product class allows access with and without
    underscores and dashes. In some cases, the restriction is due to
    Python syntax.
    
    For example, all the following notations are the same:
    * Product.sle_ha, but NOT Product.sle-ha!
    * Product["sle-ha"]
    * Product["sle_ha"]
    * Product("sle-ha"), but NOT Product("sle_ha")!

--- src/docbuild/models/product.py
+++ src/docbuild/models/product.py
@@ -1,11 +1,55 @@
-from enum import StrEnum
+from enum import EnumMeta, StrEnum
+from typing import Any
+
+# from pydantic import BaseModel, computed_field
 
 from ..constants import ALLOWED_PRODUCTS
 
 
+class StrEnumMeta(EnumMeta):
+    def __getitem__(cls, key: str) -> Any:
+        # Allow both dash and underscore access
+        candidates = [key.replace("-", "_"),
+                      key.replace("_", "-")
+                      ]
+        # print(">>> StrEnumMeta.__getitem__", cls, key, candidates)
+        for cand in candidates:
+            try:
+                return super().__getitem__(cand)
+            except KeyError:
+                continue
+
+        # If no match, raise a clear error
+        allowed = ", ".join(repr(k.value) for k in cls)
+        raise KeyError(
+            f"{key!r} is not a valid member name for {cls.__name__}. "
+            f"Allowed values: {allowed}"
+        )
+
+class BaseProductEnum(StrEnum, metaclass=StrEnumMeta):
+    @classmethod
+    def _missing_(cls, value: Any):
+        """Custom error for unknown values."""
+        allowed = ", ".join(repr(v.value) for v in cls)
+        raise ValueError(
+            f"{value!r} is not a valid {cls.__name__}. "
+            f"Allowed values are: {allowed}"
+        )
+
+    # @classmethod
+    # def attr(cls, name: str):
+    #     """Access enum members using attribute-style names with underscores."""
+    #     normalized = name.replace("-", "_")
+    #     for member in cls:
+    #         if member.value == normalized:
+    #             return member
+    #     return cls._missing_(name)
+
 # Products allows all our SUSE product names, but also "*" (=ALL).
 # We only define "ALL" as uppercase to denote a constant, the rest is lowercase.
-Product = StrEnum(
+# Product name with dashes are replace with underscores.
+# You can access "Product.sle_ha", but not "Product.sle-ha"
+Product = BaseProductEnum(
     "Product",
-    {"ALL": "*"} | {item: item.replace("-", "_") for item in ALLOWED_PRODUCTS},
+    {"ALL": "*"} | {item.replace("-", "_"): item for item in ALLOWED_PRODUCTS},
 )
--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -6,11 +6,44 @@ from docbuild.models.product import Product
 
 @pytest.mark.parametrize("product", ALLOWED_PRODUCTS)
 def test_valid_product(product):
-    instance = getattr(Product, product)
-    assert instance.name == product
+    instance = Product[product]
+    assert instance.value == product
 
 
-def test_wildcard_product():
+def test_access_all_productname_constants():
     instance = Product.ALL
     assert instance.name == "ALL"
     assert instance.value == "*"
+    assert Product["ALL"] == Product.ALL
+    assert Product("*") == Product.ALL
+
+
+def test_access_valid_productname_with_underscore():
+    assert Product["sle_ha"] == Product.sle_ha
+
+
+def test_access_valid_productname_with_dash():
+    assert Product["sle-ha"] == Product.sle_ha
+
+
+def test_access_valid_productname_uppercase_key():
+    # Enum names are case-sensitive, this should fail
+    with pytest.raises(KeyError):
+        Product["SLE-HA"]
+
+
+def test_enum_productvalue_integrity():
+    assert Product.sle_ha.value == "sle-ha"
+
+
+def test_invalid_key_raises_keyerror_with_hint():
+    with pytest.raises(KeyError) as excinfo:
+        Product["not-a-product"]
+    assert "not-a-product" in str(excinfo.value)
+
+
+def test_invalid_values_raise_value_error():
+    with pytest.raises(ValueError) as excinfo:
+        Product("unknown")
+
+    assert "unknown" in str(excinfo.value)
