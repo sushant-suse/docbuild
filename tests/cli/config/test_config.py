c195a74546885437125a47c66d62499f2b0b41f9
Tom Schraitle <tom_schr@web.de>
2025-05-31 22:32:22 +0200

    Remove test_showconfig_calls_ensure_object

--- tests/cli/config/test_config.py
+++ tests/cli/config/test_config.py
@@ -13,17 +13,3 @@ def test_showconfig_help_option(runner):
     assert result.exit_code == 0
     assert "Commands:" in result.output
     assert "env" in result.output
-
-
-def test_showconfig_calls_ensure_object(runner):
-
-    # Wrapper function to spy on ctx.ensure_object
-    @click.group()
-    @click.pass_context
-    def spy_showconfig(ctx) -> None:
-        ctx.ensure_object = MagicMock(wraps=ctx.ensure_object)
-        config(ctx)
-        ctx.ensure_object.assert_called_once_with(DocBuildContext)
-
-    result = runner.invoke(spy_showconfig, [])
-    assert result.exit_code == 0
