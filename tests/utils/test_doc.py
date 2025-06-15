856216fcb9e20f31af7475d2a65745f75b6e4798
Tom Schraitle <tom_schr@web.de>
2025-06-15 21:11:07 +0200

    Add tests for docbuild.utils.doc.docstring

---
+++ tests/utils/test_doc.py
@@ -0,0 +1,21 @@
+import pytest
+
+from docbuild.utils.doc import docstring
+
+
+def test_docstring_with_empty_func():
+    @docstring("Hello {name}, welcome to {place}", name="Alice")
+    def greet() -> str:
+        return "Hello"
+
+    assert greet() == "Hello"
+    assert greet.__doc__ == "Hello Alice, welcome to {place}"
+
+
+def test_docstring_with_existing_docstring():
+    @docstring(name="Alice")
+    def greet():
+        """Hello {name}, welcome to {place}."""
+        pass
+
+    assert greet.__doc__ == "Hello Alice, welcome to {place}."
