588dc3849cdd260af7ce77852dfa6e4bcf66fadf
Tom Schraitle <tom_schr@web.de>
2025-07-04 20:39:18 +0200

    Add a new alias "makedocs"
    * The "makedocs" alias calls "uv run --frozen make -C docs html"
      to update the documentation
    * Each alias contains now a comment about the purpose

--- devel/activate-aliases.sh
+++ devel/activate-aliases.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# For testing:
 alias upytest="uv run --frozen pytest"
+
+# For executing the project's script:
 alias docbuild="uv run --frozen docbuild"
+
+# For managing changelog and news files:
 alias towncrier="uv run --frozen towncrier"
+
+# For creating docs:
+alias makedocs="uv run --frozen make -C docs html"
